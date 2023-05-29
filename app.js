a86f7adee5af2a4d346a10f831fc316760edf8a2
Claudiu Sonica <csonica95@gmail.com>
2023-05-29 21:37:52 +0200

    design and basic functionality complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At ultrices mi tempus imperdiet nulla. Sem fringilla ut morbi tincidunt augue interdum."
 const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At ultrices mi tempus imperdiet nulla. Sem fringilla ut morbi tincidunt augue interdum."
+const posts = [];
 
 const app = express();
 
@@ -14,12 +15,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
+app.get("/", (req, res) => {
+  res.render("home", { startingContent: homeStartingContent, posts: posts }); 
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", { startingContent: aboutContent});
+})
 
+app.get("/contact", (req, res) => {
+  res.render("contact", { startingContent: contactContent });
+})
 
+app.get("/compose", (req, res) => {
+  res.render("compose");
+})
 
-
-
+app.post("/compose", (req, res) => {
+  
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
+  
+  posts.push(post);
+  res.redirect("/");
+})
 
 
 app.listen(3000, () => {
